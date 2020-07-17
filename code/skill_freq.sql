cfaa18b1f4a311f3083d0b75c2c01ac98c981e89
myaa2913 <matt.corritore@gmail.com>
2020-07-17 13:51:14 +0000

    regular update

--- code/skill_freq.sql
+++ code/skill_freq.sql
@@ -1,2 +1,2 @@
-\copy (SELECT COUNT(bgtjobid) FROM temp_skills GROUP BY	skill,skill_id) to '~/bg_skills/BGxAKM/temp/skill_freq.csv' with csv header;
+\copy (SELECT skill, skill_id,COUNT(bgtjobid) FROM temp_skills GROUP BY	skill,skill_id) to '~/bg_skills/BGxAKM/temp/skill_freq.csv' with csv header;
 
