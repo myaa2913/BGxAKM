2c688ddfdf856efb856ea8196c748983f5674fd6
myaa2913 <matt.corritore@gmail.com>
2020-07-17 20:18:47 +0000

    regular update

--- code/jobidXskillid.sql
+++ code/jobidXskillid.sql
@@ -1,14 +1,15 @@
 --create unique bgtjobid X skill_id TEMP_SKILLS sql table
-SELECT unique_skills.skill_id,
-       unique_skills.skill,
-       unique_skills.skillcluster,
-       unique_skills.skillclusterfamily,
+SELECT unique_skills_tweak.skill_id,
+       unique_skills_tweak.skill,
+       unique_skills_tweak.skillcluster,
+       unique_skills_tweak.skillclusterfamily,
        bgtjobid_skills.bgtjobid,
        maintext.soc,
        date_part('year',maintext.jobdate) AS year
 INTO temp_skills
-FROM unique_skills,bgtjobid_skills,maintext
-WHERE unique_skills.skill_id=bgtjobid_skills.skill_id AND bgtjobid_skills.bgtjobid=maintext.bgtjobid;  
+FROM unique_skills_tweak,bgtjobid_skills,maintext
+WHERE unique_skills_tweak.skill_id=bgtjobid_skills.skill_id AND bgtjobid_skills.bgtjobid=maintext.bgtjobid;  
+
 
 
 
