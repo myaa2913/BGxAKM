1893272c5dd2e6218648865d865e6a14e1c0668a
myaa2913 <matt.corritore@gmail.com>
2020-07-17 19:08:55 +0000

    regular update

--- code/updt_unique_skills_tweak_table.sql
+++ code/updt_unique_skills_tweak_table.sql
@@ -1,144 +1,144 @@
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Communication Skills' 
 WHERE skill_id = 2933;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Teamwork / Collaboration' 
 WHERE skill_id = 14427;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Organizational Skills' 
 WHERE skill_id = 10490;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Microsoft Office and Productivity Tools' 
 WHERE skill_id = 9335;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 11611;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Planning' 
 WHERE skill_id = 11196;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Basic Computer Knowledge' 
 WHERE skill_id = 3019;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Writing' 
 WHERE skill_id = 15866;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 3961;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 12431;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Physical Abilities' 
 WHERE skill_id = 11100;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Writing' 
 WHERE skill_id = 15867;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Building Effective Relationships' 
 WHERE skill_id = 1825;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Multi-Tasking' 
 WHERE skill_id = 9602;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Creativity' 
 WHERE skill_id = 3386;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Equipment Repair and Maintenance' 
 WHERE skill_id = 12403;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 14955;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Microsoft Office and Productivity Tools' 
 WHERE skill_id = 9394;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Time Management' 
 WHERE skill_id = 14668;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Communication Skills' 
 WHERE skill_id = 15300;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Leadership' 
 WHERE skill_id = 8188;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Cleaning' 
 WHERE skill_id = 2632;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Presentation Skills' 
 WHERE skill_id = 11513;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Typing' 
 WHERE skill_id = 15015;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Time Management' 
 WHERE skill_id = 9134;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 638;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Telephone Skills' 
 WHERE skill_id = 14484;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'People Management' 
 WHERE skill_id = 9170;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Communication Skills' 
 WHERE skill_id = 10443;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Analytical Skills' 
 WHERE skill_id = 3426;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'People Management' 
 WHERE skill_id = 10914;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Time Management' 
 WHERE skill_id = 11593;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Teamwork / Collaboration' 
 WHERE skill_id = 14418;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Creativity' 
 WHERE skill_id = 3383;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Basic Computer Knowledge' 
 WHERE skill_id = 1316;
 
-UPDATE temp_skills
+UPDATE unique_skills_tweak
 SET skillcluster = 'Communication Skills' 
 WHERE skill_id = 11947;
 
